ca08f3b73e4689eed5576187a508cf1b950c3c7c
harsh <66548698+harsssh@users.noreply.github.com>
2024-07-27 00:42:57 +0900

    自動計測の実行位置を変更

--- run.sh
+++ run.sh
@@ -17,10 +17,9 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-curl localhost:9000/api/group/collect
-
 # 負荷試験 & 採点開始
 if [[ $HOSTNAME != app-* ]]; then
+    curl localhost:9000/api/group/collect
     (cd benchmarker && bash ./run_k6_and_score.sh)
     if [ $? -ne 0 ]; then
         echo -e "採点フローを中断します。"
