146c7f875b1936a37f65bf7129d2219e9dd616b8
harsh <66548698+harsssh@users.noreply.github.com>
2024-07-26 23:21:17 +0900

    いらないファイルを削除

--- webapp/da-tuning-setup/scripts/install-task.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-
-sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/.local/bin
-# shellcheck disable=SC2016
-echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
